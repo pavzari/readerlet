849e9b9c1b611ed3b9774c4af822b3a30bdd30d6
pavzari <92022046+pavzari@users.noreply.github.com>
2023-12-19 17:02:28 +0000

    cli outline

---
+++ readerlet/article.py
@@ -0,0 +1,62 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urlparse, urljoin
+
+
+class Article:
+    def __init__(self, url, title, byline, content, text_content):
+        self.url = url
+        self.title = title
+        self.byline = byline
+        self.content = content
+        self.text_content = text_content
+        self.images = []
+
+    def strip_hyperlinks(self):
+        """Strip the <a> tag attributes but keep the tags and content"""
+        soup = BeautifulSoup(self.content, "html.parser")
+        for a in soup.find_all("a"):
+            for attrb in list(a.attrs.keys()):
+                del a[attrb]
+        self.content = str(soup)
+
+    def strip_images(self):
+        """Strip all the <img> tags"""
+        soup = BeautifulSoup(self.content, "html.parser")
+        for img in soup.find_all("img"):
+            img.decompose()
+        self.content = str(soup)
+
+    def download_image(self, url, images_dir):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            # html rendering issues if image name contains %!
+            filename = os.path.join(images_dir, os.path.basename(urlparse(url).path))
+            with open(filename, "wb") as file:
+                for chunk in response.iter_content(1024):
+                    file.write(chunk)
+            return filename
+        return None
+
+    def extract_images(self):
+        # This can be passed from the outsite as a temp dir or zip..
+        images_dir = "images"
+        if not os.path.exists(images_dir):
+            os.makedirs(images_dir)
+
+        soup = BeautifulSoup(self.content, "html.parser")
+
+        for img_tag in soup.find_all("img"):
+            src = img_tag.get("src")
+            if src:
+                absolute_url = urljoin(self.url, src)
+                image_path = self.download_image(absolute_url, images_dir)
+                if image_path:
+                    img_tag["src"] = image_path
+                    # make sure this is just image name. Shorten it too!
+                    self.images.append(image_path)
+                    print(f"Downloaded and replaced: {absolute_url} -> {image_path}")
+                else:
+                    print(f"Failed to download: {absolute_url}")
+        self.content = str(soup)
--- readerlet/cli.py
+++ readerlet/cli.py
@@ -1,8 +1,155 @@
+import os
 import click
+import json
+import subprocess
+from contextlib import chdir
+from article import Article
+from urllib.parse import urlparse
+
+
+def check_node_installed() -> bool:
+    # node version constraints?
+    try:
+        subprocess.run(
+            ["node", "--version"],
+            check=True,
+            capture_output=True,
+        )
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
+
+def install_npm_packages() -> None:
+    if check_node_installed():
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        javascript_dir = os.path.join(current_dir, "js")
+        node_modules_dir = os.path.join(javascript_dir, "node_modules")
+
+        if not os.path.exists(node_modules_dir):
+            with chdir(javascript_dir):
+                try:
+                    subprocess.run(
+                        ["npm", "install"],
+                        capture_output=True,
+                        text=True,
+                        check=True,
+                    )
+                    click.echo("npm install completed successfully.")
+                except subprocess.CalledProcessError:
+                    raise click.ClickException("Error running npm install.")
+    else:
+        raise click.ClickException("Node.js is not installed.")
+
+
+def extract_content(url: str) -> Article:
+    try:
+        readability = subprocess.run(
+            ["node", "js/extract_stdout.js", url],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        article_data = json.loads(readability.stdout)
+
+        if article_data:
+            title = article_data.get("title", "No Title")
+            byline = article_data.get("byline", f"{urlparse(url).netloc}")
+            content = article_data.get("content", "")
+            text_content = article_data.get("content", "")
+            return Article(url, title, byline, content, text_content)
+
+    except subprocess.CalledProcessError:
+        raise click.ClickException("Error extracting article.")
 
 
 @click.command()
 @click.version_option()
-@click.argument("arg")
-def cli(arg):
-    click.echo(f"Argument: {arg}!")
+@click.argument("url", required=True)
+@click.option(
+    "--output-epub",
+    "-e",
+    type=click.Path(),
+    help="Output EPUB file to the specified path.",
+)
+@click.option(
+    "--send-to-kindle",
+    "-k",
+    is_flag=True,
+    help="Send EPUB to Kindle if outputting to EPUB.",
+)
+@click.option(
+    "--strip-hyperlinks",
+    "-s",
+    is_flag=True,
+    default=True,
+    help="Remove hyperlinks from the content.",
+)
+@click.option(
+    "--strip-images",
+    "-i",
+    is_flag=True,
+    default=True,
+    help="Remove images from the content.",
+)
+@click.option(
+    "--output-pdf",
+    "-p",
+    type=click.Path(),
+    help="Output PDF file to the specified path.",
+)
+@click.option(
+    "--output-markdown",
+    "-m",
+    type=click.Path(),
+    help="Output Markdown file to the specified path.",
+)
+@click.option(
+    "--stdout",
+    "-o",
+    is_flag=True,
+    help="Print the plain text content to stdout (pipe-friendly).",
+)
+def cli(
+    url,
+    output_epub,
+    send_to_kindle,
+    strip_hyperlinks,
+    strip_images,
+    output_pdf,
+    output_markdown,
+    stdout,
+):
+    install_npm_packages()
+    article = extract_content(url)
+
+    article.strip_hyperlinks() if strip_hyperlinks else None
+    article.strip_images() if strip_images else None
+
+    if output_epub:
+        pass
+
+        if send_to_kindle:
+            pass
+
+    if output_pdf:
+        pass
+
+    if output_markdown:
+        pass
+
+    if stdout:
+        print(article.content)
+
+
+# print(article.title)
+# print(article.byline)
+# # print(article["byline"])
+
+# # article = strip_hyperlinks(article["content"])
+# # article = strip_images(article)
+# # article.extract_images()
+
+# # f = open("article.html", "w")
+# # f.write(article.content)
+# # f.close()
